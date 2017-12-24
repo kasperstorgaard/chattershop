532d6c3cf212e6b37f3e1a0720e42628cacac865
kasper.storgaard <kasper.storgaard@gmail.com>
2017-12-24 12:45:13 +0100

    feat: add image to cards

--- server/functions/actions/helpers.js
+++ server/functions/actions/helpers.js
@@ -2,9 +2,9 @@ exports.generateCard = function(data) {
   return {
     title: data.name,
     subtitle: 'This is just a sample subtitle',
-    formatted_text: 'Body text can include unicode characters including emoji 📱.',
+    formatted_text: data.description,
     image: {
-      image_uri: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png'
+      image_uri: data.image
     },
     buttons: [{
       'title': 'This is a button',
